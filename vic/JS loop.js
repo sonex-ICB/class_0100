964728e26867799a95378f69cd02d9e0eb61f6df
TochiVic <victoriaokara1@gmail.com>
2026-08-02 21:15:52 +0100

    Add files via upload

---
+++ vic/JS loop.js
@@ -0,0 +1,16 @@
+// FOR Loop
+for(let i = 0; i < 5;  i++) {
+    console.log("Count: " + i); //0 to 4
+}
+ // loop through arry with for
+ let numbers =[10,20,30];
+ for(let i = 0; i < numbers.length; i++) {
+    console.log(numbers[i]);
+ }
+
+ // WHILE Loop
+ let Count = 0;
+ while(Count < 3) {
+    console.log("While count: " + Count);
+    Count++; 
+ }
